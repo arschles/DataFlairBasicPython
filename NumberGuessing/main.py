f15461848812e63bca0fe91b93408de60f1f66b4
Amokstakov <amoksyak@uoguelph.ca>
2020-08-06 22:50:31 -0400

    Fix proper NumberGuessing Requirement

--- NumberGuessing/main.py
+++ NumberGuessing/main.py
@@ -3,9 +3,9 @@ import random
     
 
 class NumberGuessing:
-    def __init__(self, user_input):
+    def __init__(self, user_input, random_number):
         self.user_input = user_input 
-        self.random_number = random.randint(0,10) 
+        self.random_number = random_number 
 
         try:
             self.user_input = int(self.user_input)
@@ -14,13 +14,26 @@ class NumberGuessing:
 
     def match_numbers(self):
         if self.user_input == self.random_number:
-            return print("you selected corectly")
+            return True 
         else:
-            return print("you're wrong try again")
+            return False 
 
 if __name__ == '__main__':
-    user_input = input("Pick a number")
-    x = NumberGuessing(user_input)
-    x.match_numbers()
+    count = 0
+    random_number = random.randint(0,10)
+    while count < 3:
+        user_input = input("Pick a number")
+        x = NumberGuessing(user_input, random_number)
+        if x.match_numbers():
+            print("You got the correct number")
+            break
+
+        else:
+            if x.user_input < random_number:
+                print("you gotta guess higher")
+            else:
+                print("you gotta guess lower")
+            count +=1
+            print(f"wrong number! {count}/3")
 
 
