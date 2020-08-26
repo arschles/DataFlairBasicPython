1fdd5f7409790fa5cc31451650946fe06dab8f00
Amokstakov <amoksyak@uoguelph.ca>
2020-08-26 11:03:28 -0400

    Flask Server Connection

--- YouTubeDownloader/server/api/YouTubeConverter.py
+++ YouTubeDownloader/server/api/YouTubeConverter.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+import pytube
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -9,10 +10,13 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin()
 def URL_converter():
     try:
-        data = request.json
-        print(data)
-        return data
-        
+        url = request.json
+        print(url)
+        youtube = pytube.YouTube(url)
+        video = youtube.streams.first()
+        video.download()
+        return url
+
     except Exception as e:
         return TypeError("This is just a safe error for testing")
 
