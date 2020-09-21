c62a08dfc64e3ed34c0274451c1fc59cfebc3c17
Amokstakov <amoksyak@uoguelph.ca>
2020-09-21 11:03:39 -0400

    Adding Gunicorn and moving app.y

--- YouTubeDownloader/server/api/YouTubeConverter.py
+++ YouTubeDownloader/server/api/YouTubeConverter.py
@@ -1,42 +1,42 @@
-import re
-from flask import Flask, request, send_file
-from flask_cors import CORS, cross_origin
-import pytube
+# import re
+# from flask import Flask, request, send_file
+# from flask_cors import CORS, cross_origin
+# import pytube
 
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-
-@app.route('/converter', methods=['GET', 'POST'])
-@cross_origin()
-def URL_converter():
-
-    if request.method == "POST":
-        url = request.json
-        # try:
-        youtube = pytube.YouTube(url)
-        # except Exception as e:
-        # error = "URL недействителен в формате YouTube. Проверьте еще раз"
-        # return error
-        video = youtube.streams.first()
-
-        title = video.title
-        title = re.sub(r'[^\w]', ' ', title)
-        title = title.replace(" ", "_")
-        video.download("../Videos", title)
-
-        return title
-
-    else:
-        return "This is a miss call"
+# app = Flask(__name__)
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.route('/getFile/<video_title>')
-@cross_origin()
-def return_file(video_title):
-    try:
-        return send_file(f"../Videos/{video_title}.mp4", attachment_filename=f"{video_title}.mp4", as_attachment=True)
+# @app.route('/converter', methods=['GET', 'POST'])
+# @cross_origin()
+# def URL_converter():
 
-    except Exception as e:
-        return e
+# if request.method == "POST":
+# url = request.json
+# # try:
+# youtube = pytube.YouTube(url)
+# # except Exception as e:
+# # error = "URL недействителен в формате YouTube. Проверьте еще раз"
+# # return error
+# video = youtube.streams.first()
+
+# title = video.title
+# title = re.sub(r'[^\w]', ' ', title)
+# title = title.replace(" ", "_")
+# video.download("../Videos", title)
+
+# return title
+
+# else:
+# return "This is a miss call"
+
+
+# @app.route('/getFile/<video_title>')
+# @cross_origin()
+# def return_file(video_title):
+# try:
+# return send_file(f"../Videos/{video_title}.mp4", attachment_filename=f"{video_title}.mp4", as_attachment=True)
+
+# except Exception as e:
+# return e
--- YouTubeDownloader/server/main.py
+++
@@ -1,6 +0,0 @@
-class YTConverter:
-    def __init__(self):
-        pass
-
-if __name__ == "__main__":
-    print("This is a YouTube converter")
